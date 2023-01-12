b1b0fd607e0c64eb0d373a7b75ce859377120b2f
Kasper Falk <kafa21@student.bth.se>
2023-01-12 15:53:27 +0100

    Fixed text align and padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Logo from "../assets/Logo.svg";
+import Logo from "../assets/Logob.png";
 const Footer = () => {
   return (
-    <div className="w-screen flex align-middle justify-center bg-violet-500">
+    <div className="w-full flex align-middle justify-center bg-violet-500">
       <div className="flex flex-col justify-center p-10">
         <img src={Logo} alt="React Logo" className="w-64" />
-        <p>SparkRentals © 2022</p>
+        <p className="text-center p-4">SparkRentals © 2023</p>
       </div>
     </div>
   );
